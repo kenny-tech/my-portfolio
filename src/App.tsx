7b81bcfb374d1a5942187d41a4edf3525443bef2
kenny <Kehinde.Adegbesan@AVOSOFT.CO.UK>
2024-08-14 21:36:12 -0700

    Added nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Portfolio from './pages/Portfolio';
+import Navbar from './components/Navbar/Navbar';
 
 const App: React.FC = () => {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/portfolio" element={<Portfolio />} />
