ded3b44f017b82a34b1a15916472715a6c44c121
kenny <Kehinde.Adegbesan@AVOSOFT.CO.UK>
2024-08-15 21:50:05 -0700

    Updated portfolio

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { portfolioItems } from '../../data/portfolioItems';
 import './Portfolio.css';
 
 const Portfolio: React.FC = () => {
@@ -7,29 +8,26 @@ const Portfolio: React.FC = () => {
       <div className="container">
         <h2 className="text-center">My Portfolio</h2>
         <div className="row">
-          {[...Array(8)].map((_, index) => (
-            <div className="col-md-6 mb-4" key={index}>
+          {portfolioItems.map(item => (
+            <div className="col-md-6 mb-4" key={item.id}>
               <div className="row align-items-center">
                 <div className="col-md-6">
                   <img
-                    src={`https://via.placeholder.com/300?text=Project+${index + 1}`}
-                    alt={`Project ${index + 1}`}
+                    src={item.image}
+                    alt={item.title}
                     className="img-fluid portfolio-image"
                   />
                 </div>
                 <div className="col-md-6">
                   <div className="portfolio-text">
-                    <h3>Project {index + 1}</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin euismod tincidunt quam.</p>
-                    <a href="#" className="text-link">View Work</a>
+                    <h3>{item.title}</h3>
+                    <p>{item.description}</p>
+                    <a href={item.url} className="text-link">View Work</a>
                   </div>
                 </div>
               </div>
             </div>
           ))}
-        </div>
-        <div className="text-center">
-          <a href="/portfolio" className="btn btn-primary view-more-btn">View More</a>
         </div>
       </div>
     </section>
---
+++ src/data/portfolioItems.ts
@@ -0,0 +1,66 @@
+export const portfolioItems = [
+  {
+    id: 1,
+    title: "Project 1",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin euismod tincidunt quam.",
+    image: "/images/project1.png",
+    url: "#",
+  },
+  {
+    id: 2,
+    title: "Project 2",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin euismod tincidunt quam.",
+    image: "/images/project2.png",
+    url: "#",
+  },
+  {
+    id: 3,
+    title: "Project 3",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin euismod tincidunt quam.",
+    image: "/images/project3.png",
+    url: "#",
+  },
+  {
+    id: 4,
+    title: "Project 4",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin euismod tincidunt quam.",
+    image: "/images/project4.png",
+    url: "#",
+  },
+  {
+    id: 5,
+    title: "Project 5",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin euismod tincidunt quam.",
+    image: "/images/project5.png",
+    url: "#",
+  },
+  {
+    id: 6,
+    title: "Project 6",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin euismod tincidunt quam.",
+    image: "/images/project6.png",
+    url: "#",
+  },
+  {
+    id: 7,
+    title: "Project 7",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin euismod tincidunt quam.",
+    image: "/images/project7.png",
+    url: "#",
+  },
+  {
+    id: 8,
+    title: "Project 8",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin euismod tincidunt quam.",
+    image: "/images/project8.png",
+    url: "#",
+  },
+];
