dccbc3c91f4b21ddb575e866ac943cf241e22d7f
kenny <Kehinde.Adegbesan@AVOSOFT.CO.UK>
2024-08-18 23:31:20 -0700

    Updated portfolio

--- src/data/portfolioItems.ts
+++ src/data/portfolioItems.ts
@@ -1,7 +1,7 @@
 export const portfolioItems = [
   {
     id: 1,
-    title: "Rogerdart Customer App",
+    title: "Rogerdart App",
     description: `
       <p>A mobile app that allows customers to order food from different restaurants, which then gets delivered to them.</p>
       <ul>
@@ -10,7 +10,7 @@ export const portfolioItems = [
         <li>- I added a payment gateway so users can make payments via the app.</li>
       </ul>
     `,
-    image: "/images/rogerdart-customer.jpg",
+    image: "/images/rogerdart_customer.jpg",
     url: "#",
   },
   {
@@ -21,58 +21,88 @@ export const portfolioItems = [
       <ul>
         <li>- I built the mobile app using React Native.</li>
         <li>- I added a payment gateway so users can make payments via the app.</li>
-        <li>- I built the backend of the app.</li>
+        <li>- I developed the backend of the app.</li>
       </ul>
     `,
-    image: "/images/wisesub.png",
+    image: "/images/wisesub.jpg",
     url: "#",
   },
   {
     id: 3,
-    title: "Project 3",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin euismod tincidunt quam.",
-    image: "/images/project3.png",
-    url: "#",
+    title: "SignalADoc",
+    description: `
+      <p>A telemedicine app that connects doctors with patients.</p>
+      <ul>
+        <li>- I am part of a team that built the backend for the application.</li>
+      </ul>
+    `,
+    image: "/images/signaladoc.jpg",
+    url: "https://signaladoc.com/",
   },
   {
     id: 4,
-    title: "Project 4",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin euismod tincidunt quam.",
-    image: "/images/project4.png",
-    url: "#",
+    title: "GetriPay",
+    description: `
+      <p>A fintech app that allows you to transfer money, buy airtime and data, and pay bills.</p>
+      <ul>
+        <li>- I am part of a team that built the backend for the application.</li>
+      </ul>
+    `,
+    image: "/images/getripay.jpg",
+    url: "https://getripay.com/",
   },
   {
     id: 5,
-    title: "Project 5",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin euismod tincidunt quam.",
-    image: "/images/project5.png",
-    url: "#",
+    title: "Rogerdart Customer Web App",
+    description: `
+      <p>A web app that allows customers to order food from different restaurants, which then gets delivered to them.</p>
+      <ul>
+        <li>- I built the web app using React.</li>
+        <li>- I added a geolocation feature to the app so food can be delivered to the user location.</li>
+        <li>- I added a payment gateway so users can make payments via the app.</li>
+      </ul>
+    `,
+    image: "/images/rogerdart_web.jpg",
+    url: "",
   },
   {
     id: 6,
-    title: "Project 6",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin euismod tincidunt quam.",
-    image: "/images/project6.png",
-    url: "#",
+    title: "WiseSub Web App",
+    description: `
+      <p>A web app that allows customers to order food from different restaurants, which then gets delivered to them.</p>
+      <ul>
+        <li>- I built the web app.</li>
+        <li>- I added a geolocation feature to the app so food can be delivered to the user location.</li>
+        <li>- I added a payment gateway so users can make payments via the app.</li>
+        <li>- I developed the backend of the app.</li>
+      </ul>
+    `,
+    image: "/images/wisesub_web.jpg",
+    url: "",
   },
   {
     id: 7,
-    title: "Project 7",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin euismod tincidunt quam.",
-    image: "/images/project7.png",
-    url: "#",
+    title: "Dextrac",
+    description: `
+      <p>A mobile app that helps you track your daily expenses and income.</p>
+      <ul>
+        <li>- I built the mobile app using React Native.</li>
+        <li>- I added offline capabilities to the app.</li>
+      </ul>
+    `,
+    image: "/images/dextrac.jpg",
+    url: "https://play.google.com/store/apps/datasafety?id=com.dextrac",
   },
   {
-    id: 8,
-    title: "Project 8",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin euismod tincidunt quam.",
-    image: "/images/project8.png",
-    url: "#",
+    id: 7,
+    title: "VidSocial App",
+    description: `
+      <p>A social video app that allows you to watch and share videos.</p>
+      <ul>
+        <li>- I built the mobile app using React Native.</li>
+      </ul>
+    `,
+    image: "/images/video_app.jpg",
+    url: "",
   },
 ];
